fe363e5450a3d45df74ad54da0a1ba845b73b2d7
jmhoch <41200258+jmhoch@users.noreply.github.com>
2018-08-01 09:47:54 -0400

    Update config.php

--- config.php
+++ config.php
@@ -27,6 +27,7 @@
 //          http://www.gnu.org/copyleft/gpl.html                         //
 //                                                                       //
 ///////////////////////////////////////////////////////////////////////////
+ini_set('memory_limit', '256M');
 unset($CFG);  // Ignore this line
 global $CFG;  // This is necessary here for PHPUnit execution
 $CFG = new stdClass();
